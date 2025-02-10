1ed7f2a0c2677b22f88bb313003de6dca1e20dba
nikhilrajbhar71 <nikhil.rajbhar@quokkalabs.com>
2025-02-10 12:58:52 +0530

    completed

--- hoisting.js
+++ hoisting.js
@@ -7,3 +7,10 @@ function sayMyName() {
 console.log("variable is " + test);
 
 var test = "Hello";
+
+// const clasOjb = new Myclass(); => this will throw an error
+class Myclass {
+  constructor() {
+    this.name = "John Doe";
+  }
+}

842f7de0309b42ac523a4618f8bcc0a097bf8e4c
nikhilrajbhar71 <nikhil.rajbhar@quokkalabs.com>
2025-02-10 12:22:32 +0530

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+const nums = [10, 20, 30, 40];
+const [first, , third] = nums;
+
+console.log(first);
+console.log(third);
+
+const person = { name: "Alice", age: 25, gender: "male" };
+const { name, age } = person;
+
+console.log(name);
+console.log(age);
